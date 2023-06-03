1b57d40d273fb25b8c5beeb8310a47d34c94efd7
Miko Kato <mkato3@uw.edu>
2023-06-03 11:49:18 -0700

    adding test docs

--- test.py
+++ test.py
@@ -10,11 +10,10 @@ import pandas as pd
 import math
 
 
-def test_find_high_low_pop_density() -> None:
+def test_find_high_low_pop_density(pop_density_test: pd.DataFrame) -> None:
     """
     Tests find_high_low_pop_density function
     """
-    pop_density_test = pd.read_csv('/home/test_docs/pop_density_test.csv')
 
     assert m.find_high_low_pop_density(pop_density_test) == {
         'South Sudan': 'SSD', 'Iran': 'IRN', 'Spain': 'ESP',
@@ -24,11 +23,32 @@ def test_find_high_low_pop_density() -> None:
         }
 
 
-def test_check_validity() -> None:
+def test_check_validity(co2_test: pd.DataFrame) -> None:
     """
     Tests check_validity function
     """
-    co2 = pd.read_csv('/home/test_docs/co2.csv')
+
+    a = m.check_validity(co2_test, 'population', 'gdp', 'Test1')
+    b = m.check_validity(co2_test, 'year', 'year', 'Test2')
+
+    # Fail to reject
+    assert math.isclose(a, 0.337669, abs_tol=abs(a - 0.337669))
+    # Reject
+    assert math.isclose(b, 1.0, abs_tol=abs(b - 1.0))
+
+
+def test_temp_vs_co2(co2_test: pd.DataFrame, temp_test: pd.DataFrame) -> None:
+    """
+    Tests temp_vs_co2 function
+    """
+    #m.temp_vs_co2(co2_test, temp_test)
+
+
+def test_temp_co2_per_country() -> None:
+    """
+    Tests temp_co2_per_country function
+    """
+
 
 def test_predict_temp():
     """
@@ -53,23 +73,14 @@ def test_plot_continent_emissions():
     """
 
 
-
-
-
-
-
-    a = m.check_validity(co2, 'population', 'gdp', 'Test1')
-    b = m.check_validity(co2, 'year', 'year2', 'Test2')
-
-    # Fail to reject
-    assert math.isclose(a, 0.337669, abs_tol=abs(a - 0.337669))
-    # Reject
-    assert math.isclose(b, 1.0, abs_tol=abs(b - 1.0))
-
-
 def main():
-    test_find_high_low_pop_density()
-    test_check_validity()
+    pop_density_test = pd.read_csv(r'C:\Users\mikok\CSE163-Final-Project\test_docs\pop_density_test.csv')
+    co2_test = pd.read_csv(r'C:\Users\mikok\CSE163-Final-Project\test_docs\co2_test.csv')
+    temp_test = pd.read_csv(r'C:\Users\mikok\CSE163-Final-Project\test_docs\temp_test.csv')
+
+    test_find_high_low_pop_density(pop_density_test)
+    test_check_validity(co2_test)
+    test_temp_vs_co2(co2_test, temp_test)
 
 
 if __name__ == '__main__':
